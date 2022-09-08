7a62b44632b83102eaf63eb07d00aaaf1034d003
HibbaHindawi <hibba04@hotmail.se>
2022-09-08 11:11:09 +0200

    yes

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Arv_och_objektorientering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle triangle = new Triangle();
+            Rectangle rectangle = new Rectangle();
+            
+            Console.WriteLine("Choose one Triangle or Rectangle. Answer with t or r" + " ");
+            string shape = Console.ReadLine();
+            if (shape == "t"){
+                Console.WriteLine("triangle");
+            }
+            else if (shape == "r"){
+                Console.WriteLine("rectangle");
+            }
+            else{
+                Console.WriteLine("invalid input");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ shape.cs
@@ -0,0 +1,4 @@
+abstract class Shape{
+    protected int height;
+    protected int width;
+}
